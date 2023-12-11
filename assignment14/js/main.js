0ac94dfb385af3352a921ea50936e36e235b64b3
teylah lawson <teylah.lawson@umconnect.umt.edu>
2023-12-10 17:50:37 -0700

    new

---
+++ assignment14/js/main.js
@@ -0,0 +1,9 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var cty = c.getContext("2d");
+
+//fill text
+ctx.font = '14pt Ariel'
+
+//author
+ctx.font = '12pt Roboto'
