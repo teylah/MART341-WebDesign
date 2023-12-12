413ebfa6f4052b6414b6ab9ba0c135da83ef40d9
teylah lawson <teylah.lawson@umconnect.umt.edu>
2023-12-11 18:00:19 -0700

    new css and index.html

--- assignment14/js/main.js
+++ assignment14/js/main.js
@@ -1,9 +1,15 @@
-var c = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");
+
 var ctx = c.getContext("2d");
-var cty = c.getContext("2d");
+
+ctx.fillStyle = "grey";
+ctx.fillRect(50, 50, 300, 100);
 
 //fill text
-ctx.font = '14pt Ariel'
+ctx.font = "16px monospace"
+ctx.fillText = "black"
+ctx.strokeText('a little life', 100, 550);
 
 //author
 ctx.font = '12pt Roboto'
+ctx
